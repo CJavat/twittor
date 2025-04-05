0fe1f11a249be30df8174579868012f319e5aa56
CJavatX <cdpm98@hotmail.com>
2025-04-05 07:28:57 -0600

    Modificación de la ruta del SW

--- sw.js
+++ sw.js
@@ -6,24 +6,24 @@ const DYNAMIC_CACHE = "dynamic-v1";
 const INMUTABLE_CACHE = "inmutable-v1";
 
 const APP_SHELL = [
-  "/",
-  "/index.html",
-  "/css/style.css",
-  "/img/favicon.ico",
-  "/img/avatars/spiderman.jpg",
-  "/img/avatars/hulk.jpg",
-  "/img/avatars/ironman.jpg",
-  "/img/avatars/thor.jpg",
-  "/img/avatars/wolverine.jpg",
-  "/js/app.js",
+  // "/",
+  "index.html",
+  "css/style.css",
+  "img/favicon.ico",
+  "img/avatars/spiderman.jpg",
+  "img/avatars/hulk.jpg",
+  "img/avatars/ironman.jpg",
+  "img/avatars/thor.jpg",
+  "img/avatars/wolverine.jpg",
+  "js/app.js",
 ];
 
 const APP_SHELL_INMUTABLE = [
   "https://fonts.googleapis.com/css?family=Quicksand:300,400",
   "https://fonts.googleapis.com/css?family=Lato:400,300",
   "https://kit.fontawesome.com/2ea7eef268.js",
-  "/css/animate.css",
-  "/js/libs/jquery.js",
+  "css/animate.css",
+  "js/libs/jquery.js",
 ];
 
 self.addEventListener("install", (e) => {
